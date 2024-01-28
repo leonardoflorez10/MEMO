e03d4b7f07316bd7045fcee489b27819853134bc
leonardoflorez10 <leo.fabio.florez@bootcamp-upgrade.com>
2024-01-28 23:53:52 +0100

    memo

---
+++ src/data.js
@@ -0,0 +1,118 @@
+export const imgs = [
+  {
+    id: 1,
+    alt: 'earth',
+    img: '/img/earth.jpg',
+  },
+  {
+    id: 2,
+    alt: 'jupiter',
+    img: '/img/jupiter.jpg',
+  },
+  {
+    id: 3,
+    alt: 'mars',
+    img: '/img/mars.jpg',
+  },
+  {
+    id: 4,
+    alt: 'mercury',
+    img: '/img/mercury.jpg',
+  },
+  {
+    id: 5,
+    alt: 'saturn',
+    img: '/img/saturn.jpg',
+  },
+  {
+    id: 6,
+    alt: 'uranus',
+    img: '/img/uranus.jpg',
+  },
+  {
+    id: 7,
+    alt: 'earth',
+    img: '/img/earth.jpg',
+  },
+  {
+    id: 8,
+    alt: 'jupiter',
+    img: '/img/jupiter.jpg',
+  },
+  {
+    id: 9,
+    alt: 'mars',
+    img: '/img/mars.jpg',
+  },
+  {
+    id: 10,
+    alt: 'mercury',
+    img: '/img/mercury.jpg',
+  },
+  {
+    id: 11,
+    alt: 'saturn',
+    img: '/img/saturn.jpg',
+  },
+  {
+    id: 12,
+    alt: 'uranus',
+    img: '/img/uranus.jpg',
+  },
+];
+  // {
+  //   id: 13,
+  //   img: '/img/facebook.png',
+  //   alt: 'Facebook',
+  // },
+  // {
+  //   id: 14,
+  //   img: '/img/discord.png',
+  //   alt: 'Discord',
+  // },
+  // {
+  //   id: 15,
+  //   img: '/img/instagram.png',
+  //   alt: 'Instagram',
+  // },
+  // {
+  //   id: 16,
+  //   img: '/img/whatsapp.png',
+  //   alt: 'WhatsApp',
+  // },
+  // {
+  //   id: 17,
+  //   img: '/img/google.png',
+  //   alt: 'Google',
+  // },
+  // {
+  //   id: 18,
+  //   img: '/img/google_duo.png',
+  //   alt: 'Google Duo',
+  // },
+  // {
+  //   id: 19,
+  //   img: '/img/linkedin.png',
+  //   alt: 'Linkedin',
+  // },
+  // {
+  //   id: 20,
+  //   img: '/img/netflix.png',
+  //   alt: 'Netflix',
+  // },
+  // {
+  //   id: 21,
+  //   img: '/img/messenger.png',
+  //   alt: 'Messenger',
+  // },
+  // {
+  //   id: 22,
+  //   img: '/img/spotify.png',
+  //   alt: 'Spotify'
+  // },
+  
+
+
+
+
+
